5f71ae4fa93da26a5dbf46d904359a84a679df42
xaphaniafox <f.hosseini64@gmail.com>
2023-04-09 16:46:08 +0300

    adding initial style to the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
 const Card = () => {
   return (
-    <div className="card">
-      <h1>Testing</h1>
+    <div className="container">
+      <div className="card">
+        <img src='src/images/photosnap.svg' className="card--image" />
+        <div className="about-job">about-job</div>
+        <div className="skills">skills</div>
+      </div>
     </div>
   )
 }
