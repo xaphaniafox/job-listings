57143632f41c59ef93333074aa8822cc57ff0212
xaphaniafox <f.hosseini64@gmail.com>
2023-04-09 15:13:41 +0300

    working on scss and adding initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Card />
     </div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Testing</h1>
+    </div>
+  )
+}
+export default Card
